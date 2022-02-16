3d526df4918b91f429e65c3e3db3050d1f95475a
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-16 20:57:19 +0000

    Create property_search app.

--- dublinrealestate/settings.py
+++ dublinrealestate/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'property_search',
 ]
 
 MIDDLEWARE = [
---
+++ property_search/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PropertySearchConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'property_search'
