9b8e3d737cb60925275501c8a250e6b07f562f6d
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-16 23:45:28 +0000

    Reconfigure settings.py

--- dublinrealestate/settings/settings.py
+++ dublinrealestate/settings/settings.py
@@ -1,3 +1,17 @@
+from .settings import *
+from decouple import config
+
+DEBUG = config("DEBUG")
+SECRET_KEY = config("SECRET_KEY_TESTING")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 """
 Django settings for dublinrealestate project.
 
@@ -11,6 +25,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=nk0kq1hi)ht(md0lxrkis70se895=hcr$o@vmpyg9(jjxa@#$'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -69,18 +84,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'dublinrealestate.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
