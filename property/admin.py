efdddb608a0bd237d96a626e6422da9a66c0cf78
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-13 23:26:29 +0000

    Register models in admin.py

--- property/admin.py
+++ property/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import *
+from user.models import Profile
 
-# Register your models here.
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('property_name',)}
+    list_filter = (
+        'county',
+        'district',
+        'neighborhood',
+        'property_category',
+        'property_type',
+    )
+    list_display = (
+        'property_name',
+        'county',
+        'district',
+        'neighborhood',
+        'property_category',
+        'property_type',
+        'property_price',
+    )
+
+
+admin.site.register(Profile)
+admin.site.register(County)
+admin.site.register(District)
+admin.site.register(Neighborhood)
+admin.site.register(PropertyViewing)
+admin.site.register(PropertyViewingDate)
+admin.site.register(PropertyViewingTime)
