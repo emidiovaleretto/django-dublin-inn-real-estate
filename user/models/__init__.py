92946bdf3149b33d3ebe5d5e0a24831e21e68e17
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-13 15:01:52 +0000

    Import post_save and receiver modules.

--- user/models/__init__.py
+++ user/models/__init__.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from cloudinary.models import CloudinaryField
 
 
