cab80d91ef0fba2b6786f0e7a19175ccab208861
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-13 12:22:14 +0000

    Create Property models.

---
+++ property/models/Property.py
@@ -0,0 +1,31 @@
+from property.models import *
+
+
+class Property(models.Model):
+    slug = models.SlugField(max_length=100, null=False, unique=True)
+    property_name = models.models.CharField(max_length=50, null=False, unique=True)
+    propert_address = models.models.CharField(null=False, max_length=255)
+    neighborhood = models.ForeignKey(
+        Neighborhood, null=True, related_name='neighborhood', on_delete=models.SET_NULL)
+    district = models.ForeignKey(District, related_name='district', on_delete=models.SET_NULL)
+    county = models.ForeignKey(County, related_name='county', on_delete=models.SET_NULL)
+    description = models.models.TextField()
+    images = CloudinaryField('image', default='placeholder')
+    property_category = models.IntegerField(
+        choices=PROPERTY_CATEGORY, default=2)
+    property_type = models.IntegerField(choices=PROPERTY_TYPE, default=1)
+    property_price = models.FloatField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    metreage = models.FloatField()
+    viewing_date = models.ManyToManyField(PropertyViewingDate)
+    viewing_time = models.ManyToManyField(PropertyViewingTime)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name                        
+        
---
+++ property/models/__init__.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+PROPERTY_CATEGORY = (
+    (1, 'Sale'),
+    (2, 'Rent')
+)
+
+PROPERTY_TYPE = (
+    (1, 'Studio'),
+    (2, 'Apartment'),
+    (3, 'House')
+)
+
+from .Property import Property
