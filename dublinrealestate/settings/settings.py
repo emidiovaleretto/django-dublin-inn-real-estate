fd7aedf0152f2a76682b97de805477846203604b
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-13 23:01:11 +0000

    Import envvars from env.py

--- dublinrealestate/settings/settings.py
+++ dublinrealestate/settings/settings.py
@@ -14,6 +14,9 @@ import os
 from pathlib import Path
 from decouple import config
 
+if os.path.exists('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
