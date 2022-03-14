6e4ef83004c9b534869e8e9c9576877056b46aa4
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-14 01:54:33 +0000

    Add whitenoise dependencies to MIDDLEWARE.

--- dublinrealestate/settings/settings.py
+++ dublinrealestate/settings/settings.py
@@ -55,6 +55,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
