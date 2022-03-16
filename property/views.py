edd5514e8a9c10ee0fb8ee590b82b313ca35155e
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-16 17:26:09 +0000

    Change paginate_by number.

--- property/views.py
+++ property/views.py
@@ -7,7 +7,7 @@ class PropertyList(generic.ListView):
     model = Property
     queryset = model.objects.order_by('-created_at')
     template_name = 'properties.html'
-    paginate_by = 3
+    paginate_by = 5
 
 def property_detail(request):
     return render(request, 'property_inner.html')
