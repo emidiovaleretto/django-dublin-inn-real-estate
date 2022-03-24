3c7fdf891eff04d05f7688b738ab24aef41a47bc
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-24 17:35:42 +0000

    Add HomeView class.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from property.models import Property
 
-# Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+class HomeView(generic.ListView):
+    model = Property
+    queryset = model.objects.all()[:3]
+    template_name = 'index.html'
