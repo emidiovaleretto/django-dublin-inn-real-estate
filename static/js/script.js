f7cf1f9ac628b07c0bc4183b8b938f2d43f4d901
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-28 19:54:34 +0000

    Create a changeImage function to change property images when clicked.

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,45 @@
-const btnMobile = document.getElementById("btn-mobile");
+document.addEventListener('DOMContentLoaded', () => {
 
-function toggleMenu(event) {
-    if (event.type === 'touchstart') {
-        event.preventDefault();
-    }
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
+    const btnMobile = document.getElementById("btn-mobile");
 
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Close menu')
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Open menu')
+    function toggleMenu(event) {
+        if (event.type === 'touchstart') {
+            event.preventDefault();
+        }
+        const nav = document.getElementById('nav');
+        nav.classList.toggle('active');
+        const active = nav.classList.contains('active');
+        event.currentTarget.setAttribute('aria-expanded', active);
+
+        if (active) {
+            event.currentTarget.setAttribute('aria-label', 'Close menu')
+        } else {
+            event.currentTarget.setAttribute('aria-label', 'Open menu')
+        }
     }
+
+    btnMobile.addEventListener('click', toggleMenu);
+    btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+    $('.current_year').text(new Date().getFullYear());
+
+
+    let options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    let date = new Date();
+    let today = date.toISOString().slice(0, 10);
+    document.getElementById("time-description").innerHTML = date.toLocaleDateString("en-US", options);
+    document.getElementById("appointment-date").setAttribute("value", today);
+
+});
+
+function changeImage(imgs) {
+    let expandImg = document.getElementById("first");
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "flex";
 }
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-
-var options = {
-    weekday: 'long', 
-    year: 'numeric', 
-    month: 'long', 
-    day: 'numeric' 
-};
-var date = new Date();
-let today = date.toISOString().slice(0,10);
-document.getElementById("time-description").innerHTML = date.toLocaleDateString("en-US", options);
-document.getElementById("appointment-date").setAttribute("value", today);
