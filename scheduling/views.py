02e2b1411b511f416852bc329f2a31c65bde560b
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-28 22:42:52 +0000

    Add profile picture object in the BookAPropertyViewing class.

--- scheduling/views.py
+++ scheduling/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from property.models import Property
+from user.models import Profile
 
 
 class BookAPropertyViewing(View):
@@ -8,7 +9,9 @@ class BookAPropertyViewing(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Property.objects.filter(slug=slug)
         property = get_object_or_404(queryset)
+        profile = get_object_or_404(Profile.objects.all())
         context = {
             'property': property,
+            'profile': profile
         }
         return render(request, 'schedule-visit.html', context=context)
