d53b162177afc818b58380d02f92fa5fb185f850
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-28 22:44:04 +0000

    Add profile picture object in thePropertyView class.

--- property/views.py
+++ property/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import *
+from user.models import Profile
 
 
 class PropertyList(generic.ListView):
@@ -15,9 +16,11 @@ class PropertyView(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Property.objects.filter(slug=slug)
         property = get_object_or_404(queryset)
+        profile = get_object_or_404(Profile.objects.all())
         suggestions = Property.objects.exclude(slug=slug)[:2]
         context = {
             'property': property,
+            'profile': profile,
             'suggestions': suggestions
         }
 
