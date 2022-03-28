c6a497055415a654c2fe39b341ad7c0396242b5e
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-28 21:58:37 +0000

    Modify the HomeView classes to retrieve the user's profile picture.

--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from property.models import Property
+from user.models import Profile
 
 
 class HomeView(generic.ListView):
-    model = Property
-    queryset = model.objects.all()[:3]
-    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        properties = Property.objects.all()[:3]
+        profile = get_object_or_404(Profile.objects.all())
+
+        context = {
+            'properties': properties,
+            'profile': profile
+        }
+
+        return render(request, 'index.html', context=context)
