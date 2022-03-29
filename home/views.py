e3964deffc6e25793a7b625213c1311ead3437b7
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-29 19:47:55 +0000

    Retrieve districts & neighborhoods from db and show them to the user in the select input.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from property.models import Property
+from property.models import Property, District, Neighborhood
 from user.models import Profile
 
 
@@ -8,10 +8,14 @@ class HomeView(generic.ListView):
 
     def get(self, request, *args, **kwargs):
         properties = Property.objects.all()[:3]
+        districts = District.objects.all()
+        neigborhood = Neighborhood.objects.all()
         profile = get_object_or_404(Profile.objects.all())
 
         context = {
             'properties': properties,
+            'districts': districts,
+            'neighborhoods': neigborhood,
             'profile': profile
         }
 
