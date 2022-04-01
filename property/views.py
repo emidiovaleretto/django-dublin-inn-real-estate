e34e098f2527534c1c3514684c466987dbf03413
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-01 20:05:30 +0000

    Switch from Class Based View to Function Based View.

--- property/views.py
+++ property/views.py
@@ -1,27 +1,32 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic, View
+from django.core.paginator import Paginator
 from .models import *
 from user.models import Profile
 
 
-class PropertyList(generic.ListView):
-    model = Property
-    queryset = model.objects.order_by('-created_at')
-    template_name = 'properties.html'
-    paginate_by = 5
+def property_list(request):
+    properties = Property.objects.order_by('-created_at')
+    profile = get_object_or_404(Profile.objects.all())
+    paginator = Paginator(properties, 5)
+    page = request.GET.get('page')
+    per_page = paginator.get_page(page)
+
+    context = {
+        'properties': per_page,
+        'profile': profile
+    }
+
+    return render(request, 'properties.html', context=context)
 
 
-class PropertyView(View):
+def property_detail(request, slug):
+    property = get_object_or_404(Property.objects.filter(slug=slug))
+    profile = get_object_or_404(Profile.objects.all())
+    suggestions = Property.objects.exclude(slug=slug)[:2]
+    context = {
+        'property': property,
+        'profile': profile,
+        'suggestions': suggestions
+    }
 
-    def get(self, request, slug, *args, **kwargs):
-        queryset = Property.objects.filter(slug=slug)
-        property = get_object_or_404(queryset)
-        profile = get_object_or_404(Profile.objects.all())
-        suggestions = Property.objects.exclude(slug=slug)[:2]
-        context = {
-            'property': property,
-            'profile': profile,
-            'suggestions': suggestions
-        }
-
-        return render(request, 'property_inner.html', context=context)
+    return render(request, 'property_inner.html', context=context)
