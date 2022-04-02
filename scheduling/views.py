889c1f17b59b8147d4a27f9ee083ca7ba61583c9
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-02 14:12:55 +0000

    Create schedule a viewing function and add a decorator 'login_required' to it.

--- scheduling/views.py
+++ scheduling/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.views import View
 from property.models import Property
 from user.models import Profile
 
 
-class BookAPropertyViewing(View):
+@login_required(login_url='/auth/login')
+def schedule_a_property_viewing(request, slug):
+    property = get_object_or_404(Property, slug=slug)
+    profile = get_object_or_404(Profile.objects.all())
 
-    def get(self, request, slug, *args, **kwargs):
-        queryset = Property.objects.filter(slug=slug)
-        property = get_object_or_404(queryset)
-        profile = get_object_or_404(Profile.objects.all())
-        context = {
-            'property': property,
-            'profile': profile
-        }
-        return render(request, 'schedule-visit.html', context=context)
+    context = {
+        'property': property,
+        'profile': profile
+    }
+    return render(request, 'schedule-visit.html', context=context)
