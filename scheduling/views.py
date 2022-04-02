062d790da5a92fab1ea97caed95f1f7521ba0ae3
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-02 17:52:30 +0000

    Create a function to save a scheduled visit to the database.

--- scheduling/views.py
+++ scheduling/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.views import View
-from property.models import Property
+from property.models import Property, PropertyViewing
 from user.models import Profile
 
 
@@ -15,3 +16,20 @@ def index(request, slug):
         'profile': profile
     }
     return render(request, 'schedule-visit.html', context=context)
+
+
+def schedule_a_property_viewing(request, slug):
+    user = request.user
+    appointment_date = request.POST.get('appointment-date')
+    appointment_time = request.POST.get('appointment-time')
+    property = get_object_or_404(Property, slug=slug)
+
+    viewing = PropertyViewing(
+        user=user,
+        property=property,
+        day_for_viewing=appointment_date,
+        time_for_viewing=appointment_time
+    )
+
+    viewing.save()
+    return redirect('appointment')
