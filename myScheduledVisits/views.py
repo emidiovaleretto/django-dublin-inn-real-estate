f46ac02987e4ad93214cef2c39f26eff760af0b8
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-02 20:59:59 +0000

    Create a function to display all user appointments.

--- myScheduledVisits/views.py
+++ myScheduledVisits/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from property.models import PropertyViewing
+from user.models import Profile
 
-# Create your views here.
+
+def my_scheduled_visits(request):
+    user = request.user
+    profile = get_object_or_404(Profile.objects.all())
+    appointments = PropertyViewing.objects.filter(user=user)
+
+    context = {
+        'appointments': appointments,
+        'profile': profile
+    }
+
+    return render(request, 'my-scheduled-visits.html', context=context)
