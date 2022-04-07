e2ad521fad943dbd6e9a7c891d593b6c053c8120
Emidio Valereto <emidio.valereto@gmail.com>
2022-04-07 11:34:49 +0100

    Refactor code according to PEP8.

--- dublinrealestate/settings/production.py
+++ dublinrealestate/settings/production.py
@@ -2,14 +2,11 @@ import os
 import dj_database_url
 from .settings import *
 
-
 if os.path.exists("env.py"):
     import env
 
-DEBUG = os.environ.get("DEBUG_PRODUCTION")
+DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY_PRODUCTION")
 ALLOWED_HOSTS = [os.environ.get("HEROKU_HOST")]
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("HEROKU_DB"))
-}
+DATABASES = {'default': dj_database_url.parse(os.environ.get("HEROKU_DB"))}
