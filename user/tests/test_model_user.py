c58ddbe6e34ef2b2a6af9315aff01fc405cb936f
Emidio Valereto <emidio.valereto@gmail.com>
2022-04-08 12:48:01 +0100

    Add another test for user models.

--- user/tests/test_model_user.py
+++ user/tests/test_model_user.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.contrib.auth.models import User
 from django.db import transaction, IntegrityError
 from user.models.User import Profile
@@ -9,21 +9,29 @@ class UserModelTestCase(TestCase):
     def setUp(self):
         try:
             with transaction.atomic():
-                user = User.objects.create(username='unittest', password='123456')
+                user = User.objects.create(username='unittest',
+                                           password='123456')
                 profile = Profile.objects.get(user=user)
                 profile.save()
 
         except IntegrityError as error:
             print(f'Error while creating user. Error: {error}')
 
-        self.client = Client()
-
     def test_user_was_successfully_created_in_database(self):
         user = User.objects.first()
-        self.assertEqual(user.__str__(), 'unittest')
+        self.assertTrue(user)
+
+    def test_user_is_instance_of_user_class(self):
+        user = User.objects.first()
+        self.assertIsInstance(user, User)
 
     def test_user_profile_was_successfully_created_in_database(self):
         user = User.objects.first()
         profile = Profile.objects.get(user=user)
         self.assertEqual(profile.__str__(), user.username)
         self.assertIsNotNone(profile)
+
+    def test_role_is_instance_of_int_class(self):
+        user = User.objects.first()
+        profile = Profile.objects.get(user=user)
+        self.assertIsInstance(profile.role, int)
