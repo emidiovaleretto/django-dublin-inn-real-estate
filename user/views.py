26f88d1000fc36809dde0780190a25c3bed06d85
Emidio Valereto <emidio.valereto@gmail.com>
2022-04-08 12:49:24 +0100

    Create a get_user_profile function.

--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your views here.
+
+def get_user_profile(request):
+    ''' This function returns the user profile '''
+    username = request.GET.get('username')
+    user = get_object_or_404(User, username=username)
+    profile = Profile.objects.get(user=user)
+    profile.save()
+    context = {'profile': profile}
+    return render(request, 'profile.html', context=context)
