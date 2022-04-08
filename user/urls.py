8d3dd554e0c6820cbed24fb9cec4714c6ed903e4
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-08 17:27:10 +0000

    Create admin panel views and add a new route.

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/', views.get_user_profile, name='get_user_profile'),
+    path('profile/', views.user_profile, name='user_profile'),
+    path('admin-panel/', views.admin_panel, name='admin_panel'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from property.models import Property
 from .models import Profile
 
 
-def get_user_profile(request):
+@login_required(login_url='/auth/login')
+def user_profile(request):
     ''' This function returns the user profile '''
-    username = request.GET.get('username')
+    username = request.user
     user = get_object_or_404(User, username=username)
     profile = Profile.objects.get(user=user)
     profile.save()
     context = {'profile': profile}
-    return render(request, 'profile.html', context=context)
+    return render(request, 'user_profile.html', context=context)
+    
+
+@login_required(login_url='/auth/login')
+def admin_panel(request):
+    properties = Property.objects.order_by('-created_at')
+    paginator = Paginator(properties, 5)
+    page = request.GET.get('page')
+    per_page = paginator.get_page(page)
+
+    if request.user.is_authenticated:
+        profile = Profile.objects.filter(user=request.user).first()
+    else:
+        profile = Profile.objects.all()
+
+    context = {
+        'profile': profile,
+        'properties': per_page,
+        'pagination': per_page,
+    }    
+    
+    return render(request, 'admin_panel.html', context=context)
+
